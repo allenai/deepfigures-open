ab89390dab3a0d7cacd2497cbbd6ad13f0b10774
SampannaKahu <sampyash@gmail.com>
2019-11-25 19:01:47 -0500

    Adding more tar files.

--- deepfigures/data_generation/arxiv_pipeline.py
+++ deepfigures/data_generation/arxiv_pipeline.py
@@ -374,7 +374,25 @@ def run_on_all() -> None:
     #     tarname for tarname in file_util.iterate_s3_files(ARXIV_TAR_SRC)
     #     if os.path.splitext(tarname)[1] == '.tar'
     # ]
-    tarnames = ["s3://arxiv/src/arXiv_src_0611_001.tar", "s3://arxiv/src/arXiv_src_0611_002.tar"]
+    tarnames = ["s3://arxiv/src/arXiv_src_0003_001.tar",
+                "s3://arxiv/src/arXiv_src_0306_001.tar",
+                "s3://arxiv/src/arXiv_src_0508_002.tar",
+                "s3://arxiv/src/arXiv_src_0611_001.tar",
+                "s3://arxiv/src/arXiv_src_0611_002.tar",
+                "s3://arxiv/src/arXiv_src_0704_001.tar",
+                "s3://arxiv/src/arXiv_src_0807_001.tar",
+                "s3://arxiv/src/arXiv_src_0904_004.tar",
+                "s3://arxiv/src/arXiv_src_1001_002.tar",
+                "s3://arxiv/src/arXiv_src_1008_002.tar",
+                "s3://arxiv/src/arXiv_src_1012_006.tar",
+                "s3://arxiv/src/arXiv_src_1106_004.tar",
+                "s3://arxiv/src/arXiv_src_1110_013.tar",
+                "s3://arxiv/src/arXiv_src_1203_002.tar",
+                "s3://arxiv/src/arXiv_src_1207_004.tar",
+                "s3://arxiv/src/arXiv_src_1207_005.tar",
+                "s3://arxiv/src/arXiv_src_1210_013.tar",
+                "s3://arxiv/src/arXiv_src_1302_002.tar",
+                "s3://arxiv/src/arXiv_src_1305_007.tar"]
     # Process all papers simultaneously to avoid blocking on the ones
     # where pdflatex runs forever
     grouped_tarnames = figure_utils.ordered_group_by(
